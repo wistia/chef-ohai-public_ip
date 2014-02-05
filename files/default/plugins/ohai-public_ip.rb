fa059d3fd4e96a0d70669379780ce897b1098592
Robby Grossman <robby@freerobby.com>
2014-02-05 11:01:41 -0500

    Use IPv4 endpoint so that we never get an IPv6 response.

--- files/default/plugins/ohai-public_ip.rb
+++ files/default/plugins/ohai-public_ip.rb
@@ -1,5 +1,5 @@
 provides 'public_ip'
 
-cmd = 'curl http://icanhazip.com'
+cmd = 'curl http://ipv4.icanhazip.com'
 status, stdout, stderr = run_command(:command => cmd)
 public_ip (stdout.nil? || stdout.length < 1) ? '' : stdout
